8fe7a1aecd45216f54bfba2b11188ce0824c5ae7
G & T <100107805+gishanandtharusha@users.noreply.github.com>
2022-03-22 23:03:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater
+import requests
+
+BOT_Token="5272636171:AAF9Cpn5ydw9TDnwOg92YAhCBOlrLMHtOO0"
+updater = Updater(BOT_Token)
+
+def incoming_message_action(update, context):
+  image_url=requests.post('https://api.single-developers.software/write', headers={'Content-Type': 'application/json'}, json={"text":f"{update.message.text}"}).history[1].url
+  context.bot.sendPhoto(chat_id=update.message.chat.id, photo=image_url,
+                          reply_to_message_id=update.message.reply_to_message.message_id)
+
+updater.dispatcher.add_handler( MessageHandler(Filters.text, incoming_message_action))
+updater.start_polling()
+updater.idle()
